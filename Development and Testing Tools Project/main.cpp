e377b4e1dafa6a4e7c9230388f1d2a2459f861a9
ForeverAPigman <foreverapigman@gmail.com>
2026-08-27 19:59:16 -0400

    finished

--- Development and Testing Tools Project/main.cpp
+++ Development and Testing Tools Project/main.cpp
@@ -4,5 +4,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
+	Rectangle box(10, 5);
+	cout << "Length: " << box.getLength() << endl;
+	cout << "Width: " << box.getWidth() << endl;
+	cout << "Area: " << box.getArea() << endl;
+
+	// Try to set a negative length value
+	box.setLength(-5);
+	cout << "After setting length to -5: " << endl;
+	cout << "Length: " << box.getLength() << endl;
+	cout << "Width: " << box.getWidth() << endl;
+	cout << "Area: " << box.getArea() << endl;
+	// Try to set a negative width value
+	box.setLength(5);
+	box.setWidth(-3);
+	cout << "After setting width to -3: " << endl;
+	cout << "Length: " << box.getLength() << endl;
+	cout << "Width: " << box.getWidth() << endl;
+	cout << "Area: " << box.getArea() << endl;
 }
